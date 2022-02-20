cd15494070a2cb2926af18bf7b458bf763e2e04c
macbook <achinga.chris@gmail.com>
2022-02-20 07:24:44 +0300

    created layout structure

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode } from 'react'
+import Footer from './components/Footer'
+import MetaTags from './components/MetaTags'
+import styles from '../styles/Home.module.css'
 
 // defining components
 type Props = {
@@ -9,7 +12,11 @@ type Props = {
 const Layout = ({ children, web_name = 'Chris Achinga' }: Props) => {
   return (
     <>
-      <main>{children}</main>
+      <div className={styles.container}>
+        <MetaTags />
+        <main>{children}</main>
+        <Footer />
+      </div>
     </>
   )
 }
