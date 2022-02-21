b29ce3678a62c1da1a43cfc0bcd48ef76d536bff
macbook <achinga.chris@gmail.com>
2022-02-21 18:03:49 +0300

    footer and header

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react'
 import Footer from './components/Footer'
 import MetaTags from './components/MetaTags'
-import styles from '../styles/Home.module.css'
+import NavBar from './components/NavBar'
 
 // defining components
 type Props = {
@@ -12,8 +12,9 @@ type Props = {
 const Layout = ({ children, web_name = 'Chris Achinga' }: Props) => {
   return (
     <>
-      <div className={styles.container}>
-        <MetaTags />
+      <MetaTags />
+      <div>
+        <NavBar />
         <main>{children}</main>
         <Footer />
       </div>
